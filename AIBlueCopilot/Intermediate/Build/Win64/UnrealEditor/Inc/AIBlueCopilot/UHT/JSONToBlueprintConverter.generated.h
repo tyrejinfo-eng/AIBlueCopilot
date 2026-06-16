fc56667a3a01f3f26c63d29cc5dd20ade9c49aab
Ty Rejinfo <ty.rejinfo@gmail.com>
2026-06-16 22:43:26 +0200

    Add files via upload

---
+++ AIBlueCopilot/Intermediate/Build/Win64/UnrealEditor/Inc/AIBlueCopilot/UHT/JSONToBlueprintConverter.generated.h
@@ -0,0 +1,73 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Converter/JSONToBlueprintConverter.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+ 
+#ifdef AIBLUECOPILOT_JSONToBlueprintConverter_generated_h
+#error "JSONToBlueprintConverter.generated.h already included, missing '#pragma once' in JSONToBlueprintConverter.h"
+#endif
+#define AIBLUECOPILOT_JSONToBlueprintConverter_generated_h
+
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_SPARSE_DATA
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execParseJSONSpec); \
+	DECLARE_FUNCTION(execValidateJSONSpec); \
+	DECLARE_FUNCTION(execConvertJSONToBlueprint);
+
+
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_ACCESSORS
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUJSONToBlueprintConverter(); \
+	friend struct Z_Construct_UClass_UJSONToBlueprintConverter_Statics; \
+public: \
+	DECLARE_CLASS(UJSONToBlueprintConverter, UObject, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AIBlueCopilot"), NO_API) \
+	DECLARE_SERIALIZER(UJSONToBlueprintConverter)
+
+
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UJSONToBlueprintConverter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UJSONToBlueprintConverter(UJSONToBlueprintConverter&&); \
+	NO_API UJSONToBlueprintConverter(const UJSONToBlueprintConverter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UJSONToBlueprintConverter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UJSONToBlueprintConverter); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UJSONToBlueprintConverter) \
+	NO_API virtual ~UJSONToBlueprintConverter();
+
+
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_16_PROLOG
+#define FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_SPARSE_DATA \
+	FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_ACCESSORS \
+	FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_INCLASS_NO_PURE_DECLS \
+	FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h_19_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> AIBLUECOPILOT_API UClass* StaticClass<class UJSONToBlueprintConverter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_Ty_Documents_Unreal_Projects_AIBLUE_Plugins_AIBlueCopilot_Source_AIBlueCopilot_Public_Converter_JSONToBlueprintConverter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
